82eca70d0d0c0f1109af90b7d071c5d7df57b1b9
narasung101 <narasung101@example.com>
2019-10-10 17:57:48 +0900

    19/10/10/17:57

--- src/main/java/com/getmoney/web/HomeController.java
+++ src/main/java/com/getmoney/web/HomeController.java
@@ -31,8 +31,8 @@ public class HomeController {
 		
 		String formattedDate = dateFormat.format(date);
 		
-		//model.addAttribute("serverTime", formattedDate );
-		
+		model.addAttribute("serverTime", formattedDate );
+		//1
 		return "home";
 	}
 	
