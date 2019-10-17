099a27fbd375aa0b202fd891c7c5a47751c2ae7a
narasung101 <narasung101@example.com>
2019-10-17 19:44:52 +0900

    19/10/17 19:44

---
+++ src/main/java/com/getmoney/web/controller/CustomerController.java
@@ -0,0 +1,36 @@
+package com.getmoney.web.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.getmoney.web.domains.CustomerDTO;
+
+
+@Controller
+@RequestMapping("/customer/*")
+public class CustomerController {
+	private static final Logger Logger = LoggerFactory.getLogger(CustomerController.class);
+		
+	@PostMapping("/join")
+	public @ResponseBody Map<?,?> join(@RequestBody CustomerDTO cus){
+		Logger.info("ajax가 보낸 아이디와 비번{}", cus.getMid()+","+cus.getMpw());
+		HashMap<String,String> map = new HashMap<>();
+
+		map.put("mid", cus.getMid());
+		map.put("mpw", cus.getMpw());
+		Logger.info("map에  담긴 아이디와 비번{}", map.get("mid")+","+map.get("mpw"));
+	    return map;
+	}
+
+}
--- src/main/java/com/getmoney/web/domains/CustomerDTO.java
+++ src/main/java/com/getmoney/web/domains/CustomerDTO.java
@@ -10,5 +10,63 @@ import lombok.Data;
 public class CustomerDTO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
-	private String deptno, dname, loc;   
+	private String mid,mpw,mname,email,phonenum,birth,tooja,register_date,tier;
+	public String getMid() {
+		return mid;
+	}
+	public void setMid(String mid) {
+		this.mid = mid;
+	}
+	public String getMpw() {
+		return mpw;
+	}
+	public void setMpw(String mpw) {
+		this.mpw = mpw;
+	}
+	public String getMname() {
+		return mname;
+	}
+	public void setMname(String mname) {
+		this.mname = mname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhonenum() {
+		return phonenum;
+	}
+	public void setPhonenum(String phonenum) {
+		this.phonenum = phonenum;
+	}
+	public String getBirth() {
+		return birth;
+	}
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+	public String getTooja() {
+		return tooja;
+	}
+	public void setTooja(String tooja) {
+		this.tooja = tooja;
+	}
+	public String getRegister_date() {
+		return register_date;
+	}
+	public void setRegister_date(String register_date) {
+		this.register_date = register_date;
+	}
+	public String getTier() {
+		return tier;
+	}
+	public void setTier(String tier) {
+		this.tier = tier;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
 }
